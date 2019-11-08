02123b0a0e8d21df755742a1e859e5873e36b1e4
Lilyli963 <2109268441@qq.com>
2019-11-08 09:57:28 +0800

    根据建议进行修改，身份标识为虚拟机后缀名963，下次会添加更明显标识

--- chap0x05/py/TCP_XMAS_scan.py
+++ chap0x05/py/TCP_XMAS_scan.py
@@ -2,7 +2,7 @@
 from scapy.all import *
 
 pkt = IP(dst="172.16.111.127")/TCP(dport=RandShort(),flags="FPU")
-ret = sr1(pkt1,timeout=10)
+ret = sr1(pkt,timeout=10)
 if (str(type(ret))=="<class 'NoneType'>"):
 	print("open|Filtered")
 elif(ret.haslayer(TCP)):
